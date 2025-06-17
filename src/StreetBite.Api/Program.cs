39665bd6591bb4981f5363237abf6ef3dd9eb418
Guilherme Arcoverde <gui.arcoverde21@gmail.com>
2025-06-17 19:16:33 -0300

    add infrastructure services and database migration in Program.cs

--- src/StreetBite.Api/Program.cs
+++ src/StreetBite.Api/Program.cs
@@ -1,14 +1,19 @@
+using StreetBite.Infrastructure;
+using StreetBite.Infrastructure.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
+builder.Configuration.GetConnectionString("DbConnection");
+
+var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -19,4 +24,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+if (app.Environment.IsDevelopment())
+{
+    await MigrateDatabase();
+}
+
 app.Run();
+
+async Task MigrateDatabase()
+{
+    await using var scope = app.Services.CreateAsyncScope();
+    await DatabaseMigrations.MigrateDatabase(scope.ServiceProvider);
+}
+
+namespace StreetBite.Api
+{
+    public partial class Program;
+}
